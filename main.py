c857229b267621a38712ac0e75ea692f06a8df4b
Justin H <justin9.ij@gmail.com>
2021-10-07 17:02:58 -0400

    jhuang feat

---
+++ main.py
@@ -0,0 +1,39 @@
+import os, uuid, sys
+from azure.storage.filedatalake import DataLakeServiceClient
+from azure.core._match_conditions import MatchConditions
+from azure.storage.filedatalake._models import ContentSettings
+
+def initialize_storage_account(storage_account_name, storage_account_key):
+    
+    try:  
+        global service_client
+
+        service_client = DataLakeServiceClient(account_url="{}://{}.dfs.core.windows.net".format(
+            "https", storage_account_name), credential=storage_account_key)
+    
+    except Exception as e:
+        print(e)
+
+def upload_file_to_directory():
+    try:
+
+        file_system_client = service_client.get_file_system_client(file_system="my-file-system")
+
+        directory_client = file_system_client.get_directory_client("my-directory")
+        
+        file_client = directory_client.create_file("uploaded-file.txt")
+        local_file = open("C:\\file-to-upload.txt",'r')
+
+        file_contents = local_file.read()
+
+        file_client.append_data(data=file_contents, offset=0, length=len(file_contents))
+
+        file_client.flush_data(len(file_contents))
+
+    except Exception as e:
+      print(e)
+
+
+if __name__ == "__main__":
+    initialize_storage_account('GaN Systems Inc.','d7214b07-d87c-49bf-9b2f-36f8bd21e37f')
+    print('success')
